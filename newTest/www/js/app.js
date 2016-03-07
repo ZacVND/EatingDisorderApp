8f60fe4ac6d4e849c6c2e8cf724a087b6b959786
ZacVND <zac.luong.15@ucl.ac.uk>
2016-03-07 14:02:34 +0000

    Re-organised the controller code into controllers.js file

--- newTest/www/js/app.js
+++ newTest/www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('starter', ['ionic', 'starter.controllers'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -61,42 +61,4 @@ angular.module('starter', ['ionic'])
       }
     })
     $urlRouterProvider.otherwise('/menu/logs');
-})
-
-.controller('namesCtrl', ['$scope', '$state', function($scope, $state) {
-    $scope.whichEntry = $state.params.aId;
-
-    $scope.timeOther = false;
-
-    $scope.logs = [
-
-        {date:'15/01/2015',entries:[
-          {id:'150115b',time:'07:06',meal:'Breakfast',food:'Cereal, milk, orange juice',thoughts:'Feeling quite nervous',location:'Home',people:'Alone'},
-          {id:'150115l',time:'13:25',meal:'Lunch',food:'Ham sandwich, crisps, apple',location:'School, college, uni',people:'Friends'},
-          {id:'150115d',time:'19:02',meal:'Dinner',food:'Pasta and pesto',thoughts:'Proud of myself for eating 3 meals today',location:'Home',people:'Parents'}
-        ]},
-
-        {date:'28/02/2016',entries:[
-          {id:'280216b',time:'07:06',meal:'Breakfast',food:'Cereal, milk, orange juice',thoughts:'Feeling quite nervous',location:'Home',people:'Alone'},
-          {id:'280216l',time:'13:25',meal:'Lunch',food:'Ham sandwich, crisps, apple',location:'Home',people:'Alone'},
-          {id:'280216d',time:'19:02',meal:'Dinner',food:'Pasta and pesto',thoughts:'Proud of myself for eating 3 meals today',location:'Restaurant, cafe'},
-          {id:'280216a',time:'15:59',meal:'Afternoon Snack',food:'Packet of crisps'}
-        ]},
-
-        {date:'27/02/2016',entries:[
-          {id:'270216b',time:'09:15',meal:'Breakfast',food:'Toast, jam, milk',thoughts:'So tired',location:'Home',people:'Alone'},
-          {id:'270216d',time:'17:39',meal:'Dinner',food:'Pizza and chips',thoughts:'Feel so bad',location:'Friend\'s House',people:'Friends',purge:true}
-        ]},
-
-        {date:'25/02/2016',entries:[
-          {id:'250216b',time:'08:42',meal:'Breakfast',food:'Pancakes with butter',thoughts:'Upset I had a big breakfast',location:'Home',people:'Parents'},
-          {id:'250216a',time:'17:39',meal:'Afternoon Snack',food:'6 sausage rolls',thoughts:'Feel like a pig for having so many sausage rolls',location:'Outside',people:'Alone',binge:true}
-        ]}   
-    ];
-
-    $scope.dayToFilter = "28/02/2016";
-
-    $scope.onItemDelete = function(dayIndex, item) {
-      $scope.logs[dayIndex].entries.splice($scope.logs[dayIndex].entries.indexOf(item), 1);
-    };
-}]);
+});
--- newTest/www/js/controllers.js
+++ newTest/www/js/controllers.js
@@ -1,7 +1,37 @@
-angular.controller('namesCtrl', function($scope) {
-    $scope.names = [
-        {name:'Jani',country:'Norway'},
-        {name:'Hege',country:'Sweden'},
-        {name:'Kai',country:'Denmark'}
+angular.module('starter.controllers', [])
+
+.controller('namesCtrl', ['$scope', '$state', function($scope, $state) {
+    $scope.whichEntry = $state.params.aId;
+
+    $scope.logs = [
+
+        {date:'15/01/2015',entries:[
+          {id:'150115b',time:'07:06',meal:'Breakfast',food:'Cereal, milk, orange juice',thoughts:'Feeling quite nervous',location:'Home',people:'Alone'},
+          {id:'150115l',time:'13:25',meal:'Lunch',food:'Ham sandwich, crisps, apple',location:'School, college, uni',people:'Friends'},
+          {id:'150115d',time:'19:02',meal:'Dinner',food:'Pasta and pesto',thoughts:'Proud of myself for eating 3 meals today',location:'Home',people:'Parents'}
+        ]},
+
+        {date:'28/02/2016',entries:[
+          {id:'280216b',time:'07:06',meal:'Breakfast',food:'Cereal, milk, orange juice',thoughts:'Feeling quite nervous',location:'Home',people:'Alone'},
+          {id:'280216l',time:'13:25',meal:'Lunch',food:'Ham sandwich, crisps, apple',location:'Home',people:'Alone'},
+          {id:'280216d',time:'19:02',meal:'Dinner',food:'Pasta and pesto',thoughts:'Proud of myself for eating 3 meals today',location:'Restaurant, cafe'},
+          {id:'280216a',time:'15:59',meal:'Afternoon Snack',food:'Packet of crisps'}
+        ]},
+
+        {date:'27/02/2016',entries:[
+          {id:'270216b',time:'09:15',meal:'Breakfast',food:'Toast, jam, milk',thoughts:'So tired',location:'Home',people:'Alone'},
+          {id:'270216d',time:'17:39',meal:'Dinner',food:'Pizza and chips',thoughts:'Feel so bad',location:'Friend\'s House',people:'Friends',purge:true}
+        ]},
+
+        {date:'25/02/2016',entries:[
+          {id:'250216b',time:'08:42',meal:'Breakfast',food:'Pancakes with butter',thoughts:'Upset I had a big breakfast',location:'Home',people:'Parents'},
+          {id:'250216a',time:'17:39',meal:'Afternoon Snack',food:'6 sausage rolls',thoughts:'Feel like a pig for having so many sausage rolls',location:'Outside',people:'Alone',binge:true}
+        ]}   
     ];
-});
+
+    $scope.dayToFilter = "28/02/2016";
+
+    $scope.onItemDelete = function(dayIndex, item) {
+      $scope.logs[dayIndex].entries.splice($scope.logs[dayIndex].entries.indexOf(item), 1);
+    };
+}]);
