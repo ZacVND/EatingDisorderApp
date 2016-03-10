ab3617951e51964a4ceaf1dd195bb02359231196
Alasdair Hall <al@dhsound.co.uk>
2016-03-10 12:15:03 +0000

    Display dates in a more readable format on the logs page

--- newTest/www/js/controllers.js
+++ newTest/www/js/controllers.js
@@ -80,8 +80,85 @@ angular.module('starter.controllers', [])
       return year + "/" + month + "/" + day;
     }
 
-    $scope.getDisplayDate = function() {
-      
+    $scope.getDisplayDate = function(date) {
+      var d = new Date(date);
+      var now = new Date();
+
+      var day = getDayString(d.getDay());
+      var date = d.getDate();
+      var month = getMonthString(d.getMonth());
+
+      if(d.getFullYear() != now.getFullYear()) {
+        return day + " " + date + " " + month + " " + d.getFullYear();
+      }
+      return day + " " + date + " " + month;
+    }
+
+    getDayString = function(dayNum) {
+      switch(dayNum) {
+        case 0:
+          return "Sunday";
+          break;
+        case 1:
+          return "Monday";
+          break;
+        case 2:
+          return "Tuesday";
+          break;
+        case 3:
+          return "Wednesday";
+          break;
+        case 4:
+          return "Thursday";
+          break;
+        case 5:
+          return "Friday";
+          break;
+        case 6:
+          return "Saturday";
+          break;
+      }
+    }
+
+    getMonthString = function(monthNum) {
+      switch(monthNum) {
+        case 0:
+          return "January";
+          break;
+        case 1:
+          return "February";
+          break;
+        case 2:
+          return "March";
+          break;
+        case 3:
+          return "April";
+          break;
+        case 4:
+          return "May";
+          break;
+        case 5:
+          return "June";
+          break;
+        case 6:
+          return "July";
+          break;
+        case 7:
+          return "August";
+          break;
+        case 8:
+          return "September";
+          break;
+        case 9:
+          return "October";
+          break;
+        case 10:
+          return "November";
+          break;
+        case 11:
+          return "December";
+          break;
+      }
     }
 
     append0 = function(val) {
