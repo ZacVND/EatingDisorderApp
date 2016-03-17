7f164cd52fae980f547829e7394ff35d4e3b696b
Alasdair Hall <al@dhsound.co.uk>
2016-03-17 13:57:31 +0000

    Added the json file. This has slightly messed up the automatic date entry creation function

--- newTest/www/js/controllers.js
+++ newTest/www/js/controllers.js
@@ -43,70 +43,68 @@ angular.module('starter.controllers', [])
   $scope.people = ["Alone","Parents","Friends","Other"];
 }])
 
-.controller('namesCtrl', ['$scope', '$state', function($scope, $state) {
+.controller('namesCtrl', ['$scope', '$state', '$http', function($scope, $state, $http) {
     $scope.whichEntry = $state.params.aId;
 
-    $scope.logs = [
+    $http.get('js/data.json').success(function(data) {
+      $scope.logs = data.logs;
+    });
 
-        {date:'2015/01/15',entries:[
-          {id:'150115b',time:'07:06',meal:'Breakfast',food:'Cereal, milk, orange juice',thoughts:'Feeling quite nervous',location:'Home',people:'Alone'},
-          {id:'150115l',time:'13:25',meal:'Lunch',food:'Ham sandwich, crisps, apple',location:'School, college, uni',people:'Friends'},
-          {id:'150115d',time:'19:02',meal:'Dinner',food:'Pasta and pesto',thoughts:'Proud of myself for eating 3 meals today',location:'Home',people:'Parents'}
-        ]},
+    // $scope.logs = [
 
-        {date:'2016/03/16',entries:[
+    //     {date:'2015/01/15',entries:[
+    //       {id:'150115b',time:'07:06',meal:'Breakfast',food:'Cereal, milk, orange juice',thoughts:'Feeling quite nervous',location:'Home',people:'Alone'},
+    //       {id:'150115l',time:'13:25',meal:'Lunch',food:'Ham sandwich, crisps, apple',location:'School, college, uni',people:'Friends'},
+    //       {id:'150115d',time:'19:02',meal:'Dinner',food:'Pasta and pesto',thoughts:'Proud of myself for eating 3 meals today',location:'Home',people:'Parents'}
+    //     ]},
 
-        ]},
+    //     {date:'2016/03/16',entries:[
 
-        {date:'2016/02/28',entries:[
-          {id:'280216b',time:'07:06',meal:'Breakfast',food:'Cereal, milk, orange juice',thoughts:'Feeling quite nervous',location:'Home',people:'Alone'},
-          {id:'280216l',time:'13:25',meal:'Lunch',food:'Ham sandwich, crisps, apple',location:'Home',people:'Alone'},
-          {id:'280216d',time:'19:02',meal:'Dinner',food:'Pasta and pesto',thoughts:'Proud of myself for eating 3 meals today',location:'Restaurant, cafe'},
-          {id:'280216a',time:'15:59',meal:'Afternoon Snack',food:'Packet of crisps'}
-        ]},
+    //     ]},
 
-        {date:'2016/02/27',entries:[
-          {id:'270216b',time:'09:15',meal:'Breakfast',food:'Toast, jam, milk',thoughts:'So tired',location:'Home',people:'Alone'},
-          {id:'270216d',time:'17:39',meal:'Dinner',food:'Pizza and chips',thoughts:'Feel so bad',location:'Friend\'s House',people:'Friends',purge:true}
-        ]},
+    //     {date:'2016/02/28',entries:[
+    //       {id:'280216b',time:'07:06',meal:'Breakfast',food:'Cereal, milk, orange juice',thoughts:'Feeling quite nervous',location:'Home',people:'Alone'},
+    //       {id:'280216l',time:'13:25',meal:'Lunch',food:'Ham sandwich, crisps, apple',location:'Home',people:'Alone'},
+    //       {id:'280216d',time:'19:02',meal:'Dinner',food:'Pasta and pesto',thoughts:'Proud of myself for eating 3 meals today',location:'Restaurant, cafe'},
+    //       {id:'280216a',time:'15:59',meal:'Afternoon Snack',food:'Packet of crisps'}
+    //     ]},
 
-        {date:'2016/02/25',entries:[
-          {id:'250216b',time:'08:42',meal:'Breakfast',food:'Pancakes with butter',thoughts:'Upset I had a big breakfast',location:'Home',people:'Parents'},
-          {id:'250216a',time:'17:39',meal:'Afternoon Snack',food:'6 sausage rolls',thoughts:'Feel like a pig for having so many sausage rolls',location:'Outside',people:'Alone',binge:true}
-        ]},
+    //     {date:'2016/02/27',entries:[
+    //       {id:'270216b',time:'09:15',meal:'Breakfast',food:'Toast, jam, milk',thoughts:'So tired',location:'Home',people:'Alone'},
+    //       {id:'270216d',time:'17:39',meal:'Dinner',food:'Pizza and chips',thoughts:'Feel so bad',location:'Friend\'s House',people:'Friends',purge:true}
+    //     ]},
 
-        {date:'2016/03/09',entries:[
-          {id:'090316b',time:'07:06',meal:'Breakfast',food:'Cereal, milk, orange juice',thoughts:'Feeling quite nervous',location:'Home',people:'Alone'},
-          {id:'090316l',time:'13:25',meal:'Lunch',food:'Ham sandwich, crisps, apple',location:'Home',people:'Alone'},
-          {id:'090316d',time:'19:02',meal:'Dinner',food:'Pasta and pesto',thoughts:'Proud of myself for eating 3 meals today',location:'Restaurant, cafe'},
-          {id:'090316a',time:'15:59',meal:'Afternoon Snack',food:'Packet of crisps'}
-        ]}   
-    ];
+    //     {date:'2016/02/25',entries:[
+    //       {id:'250216b',time:'08:42',meal:'Breakfast',food:'Pancakes with butter',thoughts:'Upset I had a big breakfast',location:'Home',people:'Parents'},
+    //       {id:'250216a',time:'17:39',meal:'Afternoon Snack',food:'6 sausage rolls',thoughts:'Feel like a pig for having so many sausage rolls',location:'Outside',people:'Alone',binge:true}
+    //     ]},
+
+    //     {date:'2016/03/09',entries:[
+    //       {id:'090316b',time:'07:06',meal:'Breakfast',food:'Cereal, milk, orange juice',thoughts:'Feeling quite nervous',location:'Home',people:'Alone'},
+    //       {id:'090316l',time:'13:25',meal:'Lunch',food:'Ham sandwich, crisps, apple',location:'Home',people:'Alone'},
+    //       {id:'090316d',time:'19:02',meal:'Dinner',food:'Pasta and pesto',thoughts:'Proud of myself for eating 3 meals today',location:'Restaurant, cafe'},
+    //       {id:'090316a',time:'15:59',meal:'Afternoon Snack',food:'Packet of crisps'}
+    //     ]}   
+    // ];
 
     $scope.goals = [
           {id:'bf01',meal:'Breakfast',food:'Cereal, milk, orange juice',location:'Home',people:'Alone'},
           {id:'lu01',meal:'Lunch',food:'Ham sandwich, crisps, apple',location:'School, college, uni',people:'Friends'},
           {id:'di01',meal:'Dinner',food:'Pasta and pesto',location:'Home',people:'Parents'}
     ];
-
-    isDateInLogs = function(d) {
-      for(var i = 0; i < $scope.logs.length; i++) {
-        if($scope.logs[i].date == d) {
-          return true;
-        }
-      }
-      return false;
-    }
     
-    createDayLog = function() {
+    $scope.createDayLog = function() {
       var d = new moment();
-      if(!isDateInLogs(d.format('YYYY/MM/DD'))) {
+      if(JSON.stringify($scope.logs).indexOf(d.format('YYYY/MM/DD')) == -1) {
         $scope.logs.push({date:d.format('YYYY/MM/DD'),entries:[]});
       }
     }
 
-    createDayLog();
 
+    // if(typeof $scope.logs !== "undefined") {
+    //   createDayLog();
+    // }
+    
     $scope.sumMeals = function() {
       var total = 0;
         for(var i = 0; i <= $scope.goals.length; i++){
